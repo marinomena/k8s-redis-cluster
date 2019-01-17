4002926a889a974faff0840174532b908cbb9264
Marino Mena <marino.mena@gmail.com>
2019-01-17 15:23:48 +0000

    add namespace to cluster config script

--- 200-cluster-config.sh
+++ 200-cluster-config.sh
@@ -1,2 +1,2 @@
-kubectl exec -it redis-cluster-0 -- redis-trib create --replicas 1 $(kubectl get pods -l app=redis-cluster -o jsonpath='{range.items[*]}{.status.podIP}:6379 ')
+kubectl -n redis-cluster exec -it redis-cluster-0 -- redis-trib create --replicas 1 $(kubectl -n redis-cluster get pods -l app=redis-cluster -o jsonpath='{range.items[*]}{.status.podIP}:6379 ')
 
